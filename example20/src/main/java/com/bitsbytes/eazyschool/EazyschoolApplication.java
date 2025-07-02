4f5503705e2739752b07c2b32eb06bb793ed58ea
Tripti Singla <triptisingla306@gmail.com>
2025-07-03 01:06:38 +0530

    Hello World web app using thymeleaf and devtools demo

---
+++ example20/src/main/java/com/bitsbytes/eazyschool/EazyschoolApplication.java
@@ -0,0 +1,14 @@
+package com.bitsbytes.eazyschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class EazyschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyschoolApplication.class, args);
+	}
+
+}
