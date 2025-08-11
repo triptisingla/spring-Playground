d215752ad341cede901895a9b20749839ee63ebe
Tripti, Tripti <triptisingla306@gmail.com>
2025-08-12 01:54:09 +0530

    added Lombok

--- example22/src/main/java/com/bitsbytes/eazyschool/model/Holiday.java
+++ example22/src/main/java/com/bitsbytes/eazyschool/model/Holiday.java
@@ -1,42 +1,15 @@
 package com.bitsbytes.eazyschool.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
 
-    private String day;
-    private String reason;
-    private Type type;
+    private final String day;
+    private final String reason;
+    private final Type type;
 
     public enum Type{
         FESTIVAL,FEDERAL
-    }
-
-    public Holiday(String day, String reason, Type type){
-    this.day=day;
-    this.reason=reason;
-    this.type=type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
     }
 }
--- example22/src/main/java/com/bitsbytes/eazyschool/service/ContactService.java
+++ example22/src/main/java/com/bitsbytes/eazyschool/service/ContactService.java
@@ -2,14 +2,14 @@ package com.bitsbytes.eazyschool.service;
 
 import com.bitsbytes.eazyschool.controller.ContactController;
 import com.bitsbytes.eazyschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ContactService {
-
-    public static Logger log= LoggerFactory.getLogger(ContactService.class);
 
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved=true;
